1a7fc4b348250017520161ca0a03d199ff624bfa
Igor-Gabriel-D <igordantas.dev@gmail.com>
2026-02-18 14:52:35 -0300

    expressões booleanas com operadores lógicos, relacionais e matemáticos

---
+++ cap_05/5.2-expressoes_booleanas.py
@@ -0,0 +1,17 @@
+print(5 == 5)
+print(5 == 6)
+
+print(type(True))
+
+# Operadores relacionais
+a = 1 
+b = 2
+
+print(a > b)
+print(a < b)
+print(a <= b)
+print(a >= b)
+print(a != b)
+
+
+
---
+++ cap_05/5.3-operadores_logicos.py
@@ -0,0 +1,12 @@
+# AND
+print(False and False)
+print(True and True)
+
+# OR
+print(True or True)
+print(False or False)
+print(False or True)
+
+# NOT
+print(not(True))
+print(not(False))
